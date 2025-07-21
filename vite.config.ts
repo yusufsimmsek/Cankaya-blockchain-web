ee7b40ee40f813556b43903dedc822315752985b
yusufsimmsek <for.ysf.ai@gmail.com>
2025-07-21 17:15:33 +0300

    son hlş

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import { resolve } from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/', // Deployment için base URL ayarı
   
   // Development server configuration
   server: {
