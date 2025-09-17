4049f57192c8f9b92a0eb439f8865fcadc340790
yusufsimmsek <for.ysf.ai@gmail.com>
2025-09-17 15:53:13 +0300

    Cache-bust: favicon/og-image versiyonlarını yenile; SW cache v1.0.2

--- docs/sw.js
+++ docs/sw.js
@@ -1,7 +1,7 @@
 // Service Worker for Çankaya Blockchain Web
-// Version 1.0.0
+// Version 1.0.2
 
-const CACHE_NAME = 'cankaya-blockchain-v1.0.0'
+const CACHE_NAME = 'cankaya-blockchain-v1.0.2'
 const urlsToCache = [
   '/',
   '/index.html',
