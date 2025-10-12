fb830724921defbfb67c615800cd8d1e2d234723
yusufsimmsek <for.ysf.ai@gmail.com>
2025-10-12 03:35:37 +0300

    Deploy: Production build for GitHub Pages

---
+++ assets/js/6-52b4dbfa.js
@@ -0,0 +1,1 @@
+const s="/assets/images/6-3e06da51.png";export{s as l};
---
+++ sw.js
@@ -0,0 +1,56 @@
+// Service Worker for Çankaya Blockchain Web
+// Version 1.0.0
+
+const CACHE_NAME = 'cankaya-blockchain-v1.0.0'
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/assets/css/',
+  '/assets/js/',
+  '/assets/images/',
+  // Add specific assets that should be cached
+]
+
+// Install event
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Opened cache')
+        return cache.addAll(urlsToCache)
+      })
+  )
+  self.skipWaiting()
+})
+
+// Fetch event
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        // Return cached version or fetch from network
+        if (response) {
+          return response
+        }
+        return fetch(event.request)
+      })
+  )
+})
+
+// Activate event
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            console.log('Deleting old cache:', cacheName)
+            return caches.delete(cacheName)
+          }
+        })
+      )
+    })
+  )
+  self.clients.claim()
+}) 
